24a79fe1188324369eef54fbc8d2ce5437ae4d0f
Víctor Madrid <vitomad89@gmail.com>
2020-10-18 02:01:21 -0300

    docs agregado

---
+++ docs/precache-manifest.f71983fa0a0194cef12f3b5864903da7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79ae119340809857ac1f0ba863729ef2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "342246794de1693de009",
+    "url": "/static/css/main.7d09f939.chunk.css"
+  },
+  {
+    "revision": "ccd9761b7db298367323",
+    "url": "/static/js/2.dab6626b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.dab6626b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "342246794de1693de009",
+    "url": "/static/js/main.53cc13a1.chunk.js"
+  },
+  {
+    "revision": "0a62db85d9147c438712",
+    "url": "/static/js/runtime-main.2bd8def6.js"
+  }
+]);
